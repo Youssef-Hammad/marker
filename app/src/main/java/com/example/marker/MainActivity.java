d13b16b2d22d9aa7d9fb85df258cd7b65ae9d542
Eyad <eyadhossamm@gmail.com>
2021-04-26 17:32:41 +0200

    added splash screen, hidden action bar and added the home activity and transition from splash screen to home activity

--- app/src/main/java/com/example/marker/MainActivity.java
+++ app/src/main/java/com/example/marker/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.marker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import org.opencv.android.OpenCVLoader;
@@ -16,6 +19,18 @@ public class MainActivity extends AppCompatActivity {
         if (!OpenCVLoader.initDebug())
             Log.e("OpenCv", "Unable to load OpenCV");
         else
+        {
             Log.d("OpenCv", "OpenCV loaded");
+            final Handler handler = new Handler(Looper.getMainLooper());
+            Intent intent = new Intent(this, HomeActivity.class);
+            handler.postDelayed(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    startActivity(intent);
+                }
+            }, 1000);
+        }
     }
 }
